2b680c5064cdb6295c90d681738e002079a72919
Pablo Gamito <pablo@gamito.me>
2019-05-29 13:17:12 +0100

    Add scaffold widget to wrap app to remove underline on texts problem

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
-
 import 'package:cookoff/screens/home.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(home: HomeScreen());
+  Widget build(BuildContext context) =>
+      MaterialApp(home: Scaffold(body: HomeScreen()));
 }
