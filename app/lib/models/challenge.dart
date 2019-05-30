26806960ed47d6741bc7eda99217cf391efcb0b9
Hannes Hertach <hanneshertach@gmail.com>
2019-05-30 12:35:49 +0100

    Adapt challenge to new firebase scheme

--- app/lib/models/challenge.dart
+++ app/lib/models/challenge.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Challenge {
   final String id;
   final String owner;
-  final List<String> participants;
+  final List<Map<String, dynamic>> participants;
   final String ingredient;
   final bool complete;
   final Timestamp endTime;
@@ -12,7 +12,12 @@ class Challenge {
       this.complete, this.endTime);
 
   Challenge addParticipant(String participant) {
-    return Challenge(id, owner, participants..add(participant), ingredient,
-        complete, endTime);
+    return Challenge(
+        id,
+        owner,
+        participants..add({"name": participant, "finished": false}),
+        ingredient,
+        complete,
+        endTime);
   }
 }
