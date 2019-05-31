1602af640d58f7546532eae1b29e1844177cd645
Pablo Gamito <pablo@gamito.me>
2019-05-31 17:41:55 +0100

    Add igrendients to ingredients list

--- app/lib/screens/ingredients.dart
+++ app/lib/screens/ingredients.dart
@@ -15,31 +15,72 @@ class IngredientsScreen extends StatelessWidget {
         borderRadius: BorderRadius.only(
             topLeft: Radius.circular(38), topRight: Radius.circular(38)),
       ),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-        Container(
-          padding: EdgeInsets.only(top: 0, bottom: 30, left: 20, right: 20),
-          child: PillButton(
-            "BACK TO CHALLENGES",
-            onTap: () {
-              FragmentNavigator.pop(context);
-            },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 0, bottom: 30, left: 20, right: 20),
+            child: PillButton(
+              "BACK TO CHALLENGES",
+              onTap: () {
+                FragmentNavigator.pop(context);
+              },
+            ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.only(bottom: 15),
-          child: IngredientsSection(
-            title: 'Featured',
-            ingredients: <Ingredient>[
-              Ingredient(
-                  "cheese", "assets/ingredients/cheese.png", Color(0xFF7C54EA)),
-              Ingredient(
-                  "orange", "assets/ingredients/orange.png", Color(0xFFD0EB5C)),
-              Ingredient("cauliflower", "assets/ingredients/cauliflower.png",
-                  Color(0xFF65D2EB)),
-            ],
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: IngredientsSection(
+                      title: 'Featured',
+                      ingredients: <Ingredient>[
+                        Ingredient("cheese", "assets/ingredients/cheese.png",
+                            Color(0xFF7C54EA)),
+                        Ingredient("orange", "assets/ingredients/orange.png",
+                            Color(0xFFD0EB5C)),
+                        Ingredient(
+                            "cauliflower",
+                            "assets/ingredients/cauliflower.png",
+                            Color(0xFF65D2EB)),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: IngredientsSection(
+                      title: 'Keep it simple',
+                      ingredients: <Ingredient>[
+                        Ingredient("Bacon", "assets/ingredients/bacon.png",
+                            Color(0xFF65D2EB)),
+                        Ingredient("Fish", "assets/ingredients/fish.png",
+                            Color(0xFFEF8EBD)),
+                        Ingredient("Onion", "assets/ingredients/onion.png",
+                            Color(0xFFE3BF73)),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: IngredientsSection(
+                      title: 'Healthy eating',
+                      ingredients: <Ingredient>[
+                        Ingredient("cheese", "assets/ingredients/bacon.png",
+                            Color(0xFF65D2EB)),
+                        Ingredient("orange", "assets/ingredients/fish.png",
+                            Color(0xFFEF8EBD)),
+                        Ingredient("cauliflower",
+                            "assets/ingredients/onion.png", Color(0xFFE3BF73)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
