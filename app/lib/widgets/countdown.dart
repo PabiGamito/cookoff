a62a440cc421e70bb75ee6dc2416287f5d56f684
Pablo Gamito <pablo@gamito.me>
2019-05-31 18:33:36 +0100

    Refactor timer data structure into class

--- app/lib/widgets/countdown.dart
+++ app/lib/widgets/countdown.dart
@@ -14,10 +14,7 @@ class Countdown extends StatefulWidget {
 
 class _CountdownState extends State<Countdown> {
   final DateTime _end;
-  String _timeValue;
-  String _timeValueUnit;
-  String _timeValueAlt;
-  String _timeValueAltUnit;
+  TimeText _timeText = TimeText();
   Timer _updateTimer;
 
   _CountdownState(this._end);
@@ -43,95 +40,80 @@ class _CountdownState extends State<Countdown> {
   }
 
   void updateTimeText() {
-    _timeValue = timeText()[0][0];
-    _timeValueUnit = timeText()[1][0];
-    _timeValueAlt = timeText()[0][1];
-    _timeValueAltUnit = timeText()[1][1];
-  }
-
-  List<List<String>> timeText() {
     var _timeLeft = _end.difference(DateTime.now());
 
-    if (_timeLeft.inSeconds < 0) {
-      if (_updateTimer != null) {
-        _updateTimer.cancel();
-      }
-
-      return [
-        ['Done', ''],
-        ['', '']
-      ];
-    }
-
-    int valueCnt = 0;
-
-    var values = List<String>();
-    var units = List<String>();
-
     var days = _timeLeft.inDays;
-    if (days > 0) {
-      units.add('d');
-      values.add(_timeLeft.inDays.toString());
-      valueCnt++;
-    }
-
     var hours = _timeLeft.inHours - _timeLeft.inDays * 24;
-    if (hours > 0) {
-      units.add('h');
-      values.add(hours.toString());
-      valueCnt++;
-    }
-
     var minutes = _timeLeft.inMinutes - hours * 60 - days * 24 * 60;
-
-    if (valueCnt < 2 && minutes > 0) {
-      units.add('m');
-      values.add(minutes.toString());
-      valueCnt++;
-    }
-
     var seconds = _timeLeft.inSeconds -
         minutes * 60 -
         hours * 60 * 60 -
         days * 24 * 60 * 60;
-    if (valueCnt < 2 && seconds > 0) {
-      units.add('s');
-      values.add(seconds.toString());
-      valueCnt++;
-    }
 
-    var res = List<List<String>>();
-
-    res.add(values);
-    res.add(units);
-
-    return res;
+    _timeText.setDays(days);
+    _timeText.setHours(hours);
+    _timeText.setMinutes(minutes);
+    _timeText.setSeconds(seconds);
   }
 
   @override
-  Widget build(BuildContext context) => Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Text(
-            _timeValue ?? '',
-            style: TextStyle(
-                color: Colors.white, fontSize: 24, fontFamily: 'Montserrat'),
-          ),
-          Text(
-            (_timeValueUnit ?? '') + " ",
-            style: TextStyle(
-                color: Colors.white, fontSize: 16, fontFamily: 'Montserrat'),
-          ),
-          Text(
-            _timeValueAlt ?? '',
-            style: TextStyle(
-                color: Colors.white, fontSize: 24, fontFamily: 'Montserrat'),
-          ),
-          Text(
-            _timeValueAltUnit ?? '',
-            style: TextStyle(
-                color: Colors.white, fontSize: 16, fontFamily: 'Montserrat'),
-          ),
-        ],
+  Widget build(BuildContext context) => _timeText.getWidget();
+}
+
+class TimeText {
+  List<int> _values = [0, 0, 0, 0];
+  List<String> _units = ['d', 'h', 'm', 's'];
+
+  void setDays(int days) {
+    _values[0] = days;
+  }
+
+  void setHours(int hours) {
+    _values[1] = hours;
+  }
+
+  void setMinutes(int minutes) {
+    _values[2] = minutes;
+  }
+
+  void setSeconds(int seconds) {
+    _values[3] = seconds;
+  }
+
+  Widget getWidget() {
+    List<Widget> text = [];
+
+    if (_values.last < 0) {
+      return Text(
+        'Done',
+        style: TextStyle(
+            color: Colors.white, fontSize: 24, fontFamily: 'Montserrat'),
       );
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < _values.length; i++) {
+      if (cnt < 2 && _values[i] > 0) {
+        text.add(
+          Text(
+            _values[i].toString(),
+            style: TextStyle(
+                color: Colors.white, fontSize: 24, fontFamily: 'Montserrat'),
+          ),
+        );
+        text.add(
+          Text(
+            _units[i].toString() + " ",
+            style: TextStyle(
+                color: Colors.white, fontSize: 16, fontFamily: 'Montserrat'),
+          ),
+        );
+      }
+    }
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: text,
+    );
+  }
 }
