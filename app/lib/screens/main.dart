5cd4229c754d0c4e7ceaee1258b3b668c034b230
Hannes Hertach <hanneshertach@gmail.com>
2019-05-31 19:07:02 +0100

    Refactor authentication integration into main/home screen

--- app/lib/screens/main.dart
+++ app/lib/screens/main.dart
@@ -1,7 +1,9 @@
 import 'package:cookoff/blocs/auth_bloc.dart';
 import 'package:cookoff/models/user.dart';
+import 'package:cookoff/providers/auth_provider.dart';
 import 'package:cookoff/widgets/fragment.dart';
 import 'package:cookoff/widgets/home_header.dart';
+import 'package:cookoff/widgets/injector_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,6 +11,33 @@ import 'home.dart';
 import 'ingredients.dart';
 
 class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider authProvider =
+        InjectorWidget
+            .of(context)
+            .injector
+            .authProvider;
+    return StreamBuilder<User>(
+        stream: authProvider.profile,
+        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+          if (!snapshot.hasData) {
+            AuthBloc.instance.dispatch(NullUser());
+            return UnauthorizedMainScreen();
+          }
+          if (snapshot.data == null) {
+            AuthBloc.instance.dispatch(NullUser());
+            return UnauthorizedMainScreen();
+          } else {
+            // User is signed in
+            AuthBloc.instance.dispatch(snapshot.data);
+            return AuthorizedMainScreen();
+          }
+        });
+  }
+}
+
+class AuthorizedMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
         color: Color(0xFFFFC544),
@@ -33,4 +62,38 @@ class MainScreen extends StatelessWidget {
           ),
         ]),
       );
+}
+
+// Home screen to be rendered if the user is not signed on
+class UnauthorizedMainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider authProvider =
+        InjectorWidget
+            .of(context)
+            .injector
+            .authProvider;
+    return Container(
+        color: Color(0xFFFFC544),
+        child: Center(
+          child: GestureDetector(
+              onTap: () => authProvider.signIn(),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0xFF52C7F2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  'Sign In with Google',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: "Montserrat",
+                    color: Colors.white,
+                    letterSpacing: 2,
+                  ),
+                ),
+              )),
+        ));
+  }
 }
