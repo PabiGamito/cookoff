5e8af0babbed08ca7cca9b3aefe33f6d81519961
Pablo Gamito <pablo@gamito.me>
2019-06-04 17:32:47 +0100

    Remove debug ribbon

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -20,6 +20,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: MainScreen(),
       ),
