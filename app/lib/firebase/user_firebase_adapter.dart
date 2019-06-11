3156f359aa61feca4ad5ce8a0447e88a0fcdb92d
Hannes Hertach <hanneshertach@gmail.com>
2019-06-11 21:24:34 +0100

    Add interface to add other users as friends using email address

--- app/lib/firebase/user_firebase_adapter.dart
+++ app/lib/firebase/user_firebase_adapter.dart
@@ -14,4 +14,33 @@ class UserFirebaseAdapter implements UserProvider {
       .snapshots()
       .map((snapshot) =>
           User.fromJson(snapshot.data..['id'] = snapshot.documentID));
+
+  Future updateUser(User user) async => _firestore
+      .collection(_collection)
+      .document(user.id)
+      .updateData(user.toJson());
+
+  @override
+  Future<bool> addFriend(String email, User currentUser) async {
+    // Find friend with given email address
+    var snapshot = await _firestore
+        .collection(_collection)
+        .where("email", isEqualTo: email)
+        .snapshots()
+        .first;
+    var doc = snapshot.documents.first;
+    // If the user doesn't exist, return false
+    if (doc == null) {
+      return false;
+    }
+    User friend = User.fromJson(doc.data..['id'] = doc.documentID);
+
+    // Add each other as friends
+    currentUser.copyWithFriends([...currentUser.friends, friend.id]);
+    friend.copyWithFriends([...friend.friends, currentUser.id]);
+
+    // Update both users in database
+    Future.wait([updateUser(friend), updateUser(currentUser)]);
+    return true;
+  }
 }
--- app/lib/providers/user_provider.dart
+++ app/lib/providers/user_provider.dart
@@ -3,4 +3,7 @@ import 'package:cookoff/models/user.dart';
 abstract class UserProvider {
   // Get a user by ID
   Stream<User> userStream(String id);
+
+  // Add a friend by email. Returns if successful or not
+  Future<bool> addFriend(String email, User currentUser);
 }
