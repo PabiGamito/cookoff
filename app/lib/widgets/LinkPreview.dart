ac5f6cb3c49d311c6549f6ef3c73411e6a127430
Pablo Gamito <pablo@gamito.me>
2019-06-14 00:23:47 +0100

    Allow links to be clicked

--- app/lib/widgets/LinkPreview.dart
+++ app/lib/widgets/LinkPreview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../scalar.dart';
 
@@ -11,6 +12,14 @@ class LinkPreviewer extends StatelessWidget {
       MethodChannel('plugins.flutter.io/link_preview');
 
   LinkPreviewer({@required this.url});
+
+  _launchURL() async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -41,51 +50,54 @@ class LinkPreviewer extends StatelessWidget {
         final title = snapshot.data['title'];
         final description = snapshot.data['description'];
 
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                height: Scaler(context).scale(150),
-                width: Scaler(context).scale(150),
-                child: Image.network(
-                  imageUrl,
-                  fit: BoxFit.cover,
+        return GestureDetector(
+          onTap: _launchURL,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: Scaler(context).scale(150),
+                  width: Scaler(context).scale(150),
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.only(bottom: Scaler(context).scale(10)),
-                      child: Text(
-                        title,
-                        maxLines: 1,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: Scaler(context).scale(20),
-                          fontWeight: FontWeight.bold,
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.only(bottom: Scaler(context).scale(10)),
+                        child: Text(
+                          title,
+                          maxLines: 1,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: Scaler(context).scale(20),
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      description,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: Scaler(context).scale(12.0),
+                      Text(
+                        description,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: Scaler(context).scale(12.0),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
